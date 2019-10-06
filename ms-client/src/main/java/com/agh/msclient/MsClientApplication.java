ccef476c240e71a587f85899a1c3f4ce7021654e
aghilas <aghilas.djema@ext.leroymerlin.fr>
2019-10-06 15:34:30 +0200

    config all ms as eureka clients

--- ms-client/src/main/java/com/agh/msclient/MsClientApplication.java
+++ ms-client/src/main/java/com/agh/msclient/MsClientApplication.java
@@ -2,10 +2,12 @@ package com.agh.msclient;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableFeignClients("com.agh.msclient")
+@EnableDiscoveryClient
 public class MsClientApplication {
 
 	public static void main(String[] args) {
